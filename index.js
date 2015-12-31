7e7ce529617e2a5df148e0062748454c1e650832
bloodyKnuckles <treenopie@gmail.com>
2015-12-30 19:29:39 -0600

    fixed empty cookie glitch

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.getCookieObject = function (cookie, name) {
     var cookiepair = item.split(/=(.+)?/)
     if ( 'string' === typeof name ) {
       if ( name === cookiepair[0] ) {
-        ret = cookiepair[1].charAt(0).match(/[{"']/)
+        ret = cookiepair[1] && cookiepair[1].charAt(0).match(/[{"']/)
           ? JSON.parse(cookiepair[1])
           : cookiepair[1]
       }
@@ -14,11 +14,11 @@ exports.getCookieObject = function (cookie, name) {
       (undefined === name && 2 <= cookiepair.length)
       || (Array.isArray(name) && -1 !== name.indexOf(cookiepair[0]))
     ) {
-      ret[cookiepair[0]] = cookiepair[1].charAt(0).match(/[{"']/)
+      ret[cookiepair[0]] = cookiepair[1] && cookiepair[1].charAt(0).match(/[{"']/)
         ? JSON.parse(cookiepair[1])
         : cookiepair[1]
     }
   })
-  return ret || {}
+  return 0 < Object.keys(ret).length? ret: undefined
 }
 
